044bb29cde8ef5cff9ad3971d6a809469cb493fe
spa542 <ryan11291129@gmail.com>
2020-03-17 16:16:09 -0400

    Added template for html,js,css... Need to edit and make own and continue to work  on it

--- PythonCoursePrograms/WebDevelopment/WebServer/server.py
+++ PythonCoursePrograms/WebDevelopment/WebServer/server.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def my_home():
     return render_template('index.html');
 
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
